ba0388a85387456672174cdc780022bc02fdc8bf
nyambogahezron <hezzyn1@gmail.com>
2025-01-06 03:24:09 +0300

    add dropdown menu to CustomHeader for settings and help options

--- src/components/ui/CustomHeader.tsx
+++ src/components/ui/CustomHeader.tsx
@@ -1,18 +1,36 @@
 import { Colors } from '@/constants/Colors';
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import Entypo from '@expo/vector-icons/Entypo';
 
 export default function CustomHeader() {
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
   return (
     <View style={styles.header}>
       <Text style={styles.headerText}>Status Saver</Text>
-      <Entypo
-        name='dots-three-vertical'
-        size={20}
-        color='black'
-        style={styles.icon}
-      />
+      <TouchableOpacity onPress={toggleDropdown}>
+        <Entypo
+          name={dropdownVisible ? 'cross' : 'dots-three-vertical'}
+          size={20}
+          color='black'
+          style={styles.icon}
+        />
+      </TouchableOpacity>
+      {dropdownVisible && (
+        <View style={styles.dropdownMenu}>
+          <TouchableOpacity onPress={toggleDropdown} style={styles.menuItem}>
+            <Text style={styles.menuText}>Settings</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={toggleDropdown} style={styles.menuItem}>
+            <Text style={styles.menuText}>Help</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 }
@@ -35,5 +53,28 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 20,
     right: -5,
+  },
+  dropdownMenu: {
+    position: 'absolute',
+    width: 120,
+    top: 40,
+    right: 20,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+    zIndex: 1,
+  },
+  menuItem: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  menuText: {
+    color: Colors.black2,
+    fontFamily: 'Rb-Regular',
   },
 });
